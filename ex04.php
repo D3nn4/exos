0acb9ab643b63c3e56acfdd0c54543df54403fda
Denna <marjorie.lv@gmail.com>
2016-03-18 08:04:47 -0700

    modified ex04 add ex06

--- ex04.php
+++ ex04.php
@@ -1,10 +1,11 @@
 #!/usr/bin/php
 <?php
-
-$array = $argv;
-$length = count($array);
-for($i = 1; $i < $length; $i++){
-	echo $array[$i] . "\n";
+if (count($argv) >1 ){
+	$array = $argv;
+	$length = count($array);
+	for($i = 1; $i < $length; $i++){
+		echo $array[$i] . "\n";
+	}
 }
 
 ?>
