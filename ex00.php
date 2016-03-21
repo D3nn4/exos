f7793a87f3b6d5c29438d4ae7764fded84f3c9b2
Denna <marjorie.lv@gmail.com>
2016-03-21 19:07:09 +0000

    add ex00 j01

--- ex00.php
+++ ex00.php
@@ -1,4 +1,19 @@
 #!/usr/bin/php
 <?php
-Echo "Hello World\n";
+if (count($argv) >1 ){
+	$str = $argv[1];
+	$str = trim($str);
+	$str = str_replace('\t', ' ',$str);
+	while (true){
+		$str1 = str_replace('  ', ' ',$str);
+		if ($str1 === $str){
+			break;
+		}
+		else{
+			$str = $str1;
+		}
+	}
+	echo $str . "\n";
+	}
+}
 ?>
